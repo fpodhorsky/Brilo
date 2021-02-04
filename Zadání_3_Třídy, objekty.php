b5bc53f9f666a6894c54c1858c189fc3fae0483a
fpodhorsky <78384409+fpodhorsky@users.noreply.github.com>
2021-02-04 12:27:45 +0100

    Update Zadání_3_Třídy, objekty.php

--- Zadání_3_Třídy, objekty.php
+++ Zadání_3_Třídy, objekty.php
@@ -1,71 +1,72 @@
 <?php
-	class employee {	// vytvoření třídy
-		private $jmeno;
-		private $prijmeni;
+	class employee {	
+		private $name;
+		private $surname;
 		private $email;
 		private $phone;
 		private $id;
 
-		public function __construct($jmeno, $prijmeni, $email, $phone, $id) {
-			$this->jmeno = $jmeno;
-			$this->prijmeni = $prijmeni;
+		public function __construct(string $name, string $surname, string $email, string $phone, string $id) {
+			$this->name = $name;
+			$this->surname = $surname;
 			$this->email = $email;
 			$this->id = $id;
 			$this->phone = $phone;
 		}
 
-		//výpis informací o zaměstnanci
+		//list of informations about employee
 
 		public function introduction() {
 			echo '<br>';
 			echo '<h4>Informace o zaměstnanci č. ' . $this->id . '</h4>';
 			echo '<li>';
-			echo $this->jmeno;
+			echo $this->name;
 			echo ' ';
-			echo $this->prijmeni;
+			echo $this->surname;
 			echo '<br><li>';
 			echo $this->email;	
 			echo '<br><li>';
 			echo $this->phone;
 		}
 
-		//get - získání emailu
+		//get - getting an email
 
-		public function getEmail() {
+		public function getEmail(string $email) {
 			return $this->email;
 		}
 
-		//set - změna emailu
+		//set - email change
 
-		public function setEmail($email) {
+		public function setEmail(string $email) {
 			if (strpos($email, '@') > -1) {
 				$this->email = $email;
 			}
 		}
 
-		//set - změna tel. čísla
+		//set - phone number change
 
-		public function setPhone($phone) {
+		public function setPhone(string $phone) {
 			if(strlen($phone)==9) {
 				$this->phone = $phone;
+				return $this->phone;
 			}
 		}
 		
 	}
 
-//pole se zaměstnanci
+//array with employees
 	
-	$employees = array('1' => new empLoyee('Jan', 'Novák', 'jannovak@gmail.com', '731568555' ,'1'), 
-							  new employee('Petr', 'Rychlý', 'petrrychly@seznam.cz', '604258556' ,'2'),
-						 	  new employee('Lenka', 'Novotný', 'lenka.novotna@email.cz', '605021033' ,'3'));
+	$employees['1'] = new empLoyee('Jan', 'Novák', 'jannovak@gmail.com', '731568555' ,'1'); 
+	$employees[] = new employee('Petr', 'Rychlý', 'petrrychly@seznam.cz', '604258556' ,'2');
+	$employees[] = new employee('Lenka', 'Novotný', 'lenka.novotna@email.cz', '605021033' ,'3');
 
 
-	$employees['3']->setPhone('731011498'); //změna tel. čísla
-	$employees['2']->setEmail('peta@gmail.com'); //změna emailu
+	$employees['3']->setPhone('731011498'); //phone number change
+	$employees['2']->setEmail('peta@gmail.com'); //email change
 
- //představení všech zaměstnanců
+ //introduction of all employees
 
-	$employees['1']->introduction();
-	$employees['2']->introduction();
-	$employees['3']->introduction();
+	foreach ($employees as $employee) {
+			$employee->introduction();
+			}
 ?>
